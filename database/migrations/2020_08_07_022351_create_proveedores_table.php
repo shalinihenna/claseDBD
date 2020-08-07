596956f47a760dde6db4ecea74d3771e8cefc2c0
vagrant <vagrant@homestead>
2020-08-07 02:27:22 +0000

    ADD: Agregando migraciones

--- database/migrations/2020_08_07_022351_create_proveedores_table.php
+++ database/migrations/2020_08_07_022351_create_proveedores_table.php
@@ -15,6 +15,8 @@ class CreateProveedoresTable extends Migration
     {
         Schema::create('proveedores', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('telefono');
             $table->timestamps();
         });
     }
